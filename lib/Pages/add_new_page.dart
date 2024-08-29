81f6d43965c7bae608d928ba6c23f03fd842097c
Heshara13 <nirmithaheshara527@gmail.com>
2024-08-29 23:36:09 +0530

    Add new page and add exercise card

--- lib/Pages/add_new_page.dart
+++ lib/Pages/add_new_page.dart
@@ -1,3 +1,9 @@
+import 'package:fifth_app_workout_planner/Constants/colors.dart';
+import 'package:fifth_app_workout_planner/Constants/constant_values.dart';
+import 'package:fifth_app_workout_planner/Data/exercise_data.dart';
+import 'package:fifth_app_workout_planner/Data/user_data.dart';
+import 'package:fifth_app_workout_planner/Models/exercise_model.dart';
+import 'package:fifth_app_workout_planner/widgets/add_exercise_card.dart';
 import 'package:flutter/material.dart';
 
 class AddNewPage extends StatefulWidget {
@@ -8,13 +14,66 @@ class AddNewPage extends StatefulWidget {
 }
 
 class _AddNewPageState extends State<AddNewPage> {
+  final userData = user; // import the user
+  final exerciseList = ExerciseData().exerciseList; //exerciseList
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Center(
-            child: Text("Add new page"),
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello, ${userData.fullName}",
+                  style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: kMainBlackColor),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  "Let's add Some workouts and Equipment for today!",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: kMainColor),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  "All Exercises",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: kMainBlackColor),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                //list view to scroll horizontally
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.36,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: exerciseList.length,
+                    itemBuilder: (context, index) {
+                      Exercise exercise = exerciseList[index];
+                      return AddExerciseCard(
+                          exerciseTitle: exercise.exerciseName,
+                          noOfMinutes: exercise.noOfMinuites,
+                          exerciseImageUrl: exercise.exerciseImageUrl
+                          );
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/add_exercise_card.dart
@@ -0,0 +1,111 @@
+import 'package:fifth_app_workout_planner/Constants/colors.dart';
+import 'package:fifth_app_workout_planner/Constants/constant_values.dart';
+import 'package:flutter/material.dart';
+
+class AddExerciseCard extends StatefulWidget {
+  final String exerciseTitle;
+  final String exerciseImageUrl;
+  final int noOfMinutes;
+  const AddExerciseCard({
+    super.key, 
+    required this.exerciseTitle, 
+    required this.noOfMinutes, 
+    required this.exerciseImageUrl});
+
+  @override
+  State<AddExerciseCard> createState() => _AddExerciseCardState();
+}
+
+class _AddExerciseCardState extends State<AddExerciseCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      //height: 200,
+      margin: EdgeInsets.only(right: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: kCardBgColor,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 2),
+            blurRadius: 2,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding),
+        child: Column(
+          children: [
+            Text(
+              widget.exerciseTitle,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Image.asset(
+              widget.exerciseImageUrl,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "${widget.noOfMinutes.toString()} Minutes",
+              style: const TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 15,
+                  color: kSubtitleColor),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: kSubtitleColor.withOpacity(0.2)),
+                    child: Center(
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.add,
+                            size: 30,
+                            color: kMainDarkBlueColor,
+                          )),
+                    ),
+                  ),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: kSubtitleColor.withOpacity(0.2)),
+                    child: Center(
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border,
+                            size: 30,
+                            color: kMainpinkColor,
+                          )),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
